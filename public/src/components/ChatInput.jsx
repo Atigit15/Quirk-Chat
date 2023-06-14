090452931218b193513f1cbb0059d8c93e5983de
Atishay Jain <Atigit15@users.noreply.github.com>
2023-06-14 21:20:16 +0530

    add logout to welcome && remove emoji && connect to monogdb atlas

--- public/src/components/ChatInput.jsx
+++ public/src/components/ChatInput.jsx
@@ -1,21 +1,10 @@
 import React, { useState } from "react";
-import { BsEmojiSmileFill } from "react-icons/bs";
+import { GrAttachment} from "react-icons/gr";
 import { IoMdSend } from "react-icons/io";
 import styled from "styled-components";
-import Picker from "emoji-picker-react";
 
 export default function ChatInput({ handleSendMsg }) {
   const [msg, setMsg] = useState("");
-  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
-  const handleEmojiPickerhideShow = () => {
-    setShowEmojiPicker(!showEmojiPicker);
-  };
-
-  const handleEmojiClick = (event, emojiObject) => {
-    let message = msg;
-    message += emojiObject.emoji;
-    setMsg(message);
-  };
 
   const sendChat = (event) => {
     event.preventDefault();
@@ -29,14 +18,15 @@ export default function ChatInput({ handleSendMsg }) {
     <Container>
       <div className="button-container">
         <div className="emoji">
-          <BsEmojiSmileFill onClick={handleEmojiPickerhideShow} />
-          {showEmojiPicker && <Picker onEmojiClick={handleEmojiClick} />}
+          <GrAttachment color="white"></GrAttachment>
+          {/* <BsEmojiSmileFill onClick={handleEmojiPickerhideShow} /> */}
+          {/* {showEmojiPicker && <Picker onEmojiClick={handleEmojiClick} />} */}
         </div>
       </div>
       <form className="input-container" onSubmit={(event) => sendChat(event)}>
         <input
           type="text"
-          placeholder="type your message here"
+          placeholder="Type your message here"
           onChange={(e) => setMsg(e.target.value)}
           value={msg}
         />
@@ -67,34 +57,9 @@ const Container = styled.div`
       position: relative;
       svg {
         font-size: 1.5rem;
-        color: #ffff00c8;
+        // color: #fff;
         cursor: pointer;
       }
-      .emoji-picker-react {
-        position: absolute;
-        top: -350px;
-        background-color: #080420;
-        box-shadow: 0 5px 10px #9a86f3;
-        border-color: #9a86f3;
-        .emoji-scroll-wrapper::-webkit-scrollbar {
-          background-color: #080420;
-          width: 5px;
-          &-thumb {
-            background-color: #9a86f3;
-          }
-        }
-        .emoji-categories {
-          button {
-            filter: contrast(0);
-          }
-        }
-        .emoji-search {
-          background-color: transparent;
-          border-color: #9a86f3;
-        }
-        .emoji-group:before {
-          background-color: #080420;
-        }
       }
     }
   }
